521761c3f8da909dbcc17dc7aee5a8a94c93078c
Kung16845 <varit16845@gmail.com>
2026-07-02 17:26:08 +0700

    [T26-04366] add field partner_code

---
+++ spc_bs_apps/bs_contact_add_field/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Contact Add Field",
+    "category": "Sales/CRM",
+    "summary": "Add Partner Code field on Contacts",
+    "author": "Basic Solution Co., Ltd",
+    "version": "17.0.1.0",
+    "website": "https://www.basic-solution.com/",
+    "depends": ["base", "contacts", "partner_group"],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    "installable": True,
+    "license": "LGPL-3",
+}
