df616878c457db56d2cb2c85fcd989e0057fcb1b
Kung16845 <varit16845@gmail.com>
2026-07-03 09:47:49 +0700

    [T26-04365] Add Field in billing

---
+++ spc_bs_apps/bs_billing_add_field/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Bs Billing Add Field",
+    "category": "Accounting",
+    "summary": "Add field on Billing",
+    "author": "Basic Solution Co., Ltd",
+    "version": "17.0.1.0",
+    "website": "https://www.basic-solution.com/",
+    "depends": ["base", "account_billing"],
+    "data": [
+        "views/account_billing_view.xml",
+    ],
+    "installable": True,
+    "license": "LGPL-3",
+}
