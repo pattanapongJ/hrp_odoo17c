0c2a00908a226ece6ab54266032fb7bd4057fcd0
Kung16845 <varit16845@gmail.com>
2026-07-06 09:51:45 +0700

    [T26-04250] Report Invoice Preprint

---
+++ spc_bs_apps/bs_create_form_tax_invoice_hrp_preprint/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "BS Create Form Tax Invoice HRP Preprint",
+    "version": "17.0.0.1",
+    "author": "Basic-Solution Co., Ltd.",
+    "maintainer": "varit",
+    "website": "https://www.basic-solution.com/",
+    "category": "account",
+    "depends": ["account"],
+    "data": [
+        "report/components/report_header.xml",
+        "report/components/report_main_content.xml",
+        "report/components/report_footer.xml",
+        "report/report_invoice.xml",
+        "report/request_report.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3",
+}
