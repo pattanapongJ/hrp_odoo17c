967bcf7ffde7d3f900ae7a067e8ccf8ca5e53496
Kung16845 <varit16845@gmail.com>
2026-07-13 14:51:46 +0700

    [T26-04250] Modify Report And Add Module

---
+++ spc_bs_apps/bs_invoice_modify_field/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Bs Invoice Modify Field",
+    "category": "Accounting",
+    "summary": "Add Apply Down Payment field on Invoice Line",
+    "author": "Basic Solution Co., Ltd",
+    "version": "17.0.1.0",
+    "website": "https://www.basic-solution.com/",
+    "depends": ["account"],
+    "data": [
+        "views/account_move_views.xml",
+    ],
+    "installable": True,
+    "license": "LGPL-3",
+}
