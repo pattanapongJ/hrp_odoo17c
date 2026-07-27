008da253bb484df801cb1f0ab8b07746851860c1
Kung16845 <varit16845@gmail.com>
2026-07-27 16:19:30 +0700

    [T26-04578] Modify Report

--- spc_bs_apps/bs_create_form_quotation_g_plus/__manifest__.py
+++ spc_bs_apps/bs_create_form_quotation_g_plus/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "BS Create Form Quotation G Plus",
-    "version": "17.0.0.8",
+    "version": "17.0.0.9",
     "author": "Basic-Solution Co., Ltd.",
     "maintainer": "varit",
     "website": "https://www.basic-solution.com/",
