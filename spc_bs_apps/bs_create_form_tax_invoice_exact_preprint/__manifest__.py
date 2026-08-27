caaa92812f3b0e3110543a8efbc3edfa651095ce
Kung16845 <varit16845@gmail.com>
2026-08-27 11:17:51 +0700

    [T26-04538] Create Report Invoice exact preprint

---
+++ spc_bs_apps/bs_create_form_tax_invoice_exact_preprint/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "BS Create Form Tax Invoice Exact Preprint",
+    "version": "17.0.0.1",
+    "author": "Basic-Solution Co., Ltd.",
+    "maintainer": "varit",
+    "website": "https://www.basic-solution.com/",
+    "category": "account",
+    "depends": ["account", "l10n_th_account_tax", "l10n_th_partner", "bs_contact_add_field"],
+    "data": [
+        "report/components/report_header.xml",
+        "report/components/report_main_content.xml",
+        "report/components/report_footer.xml",
+        "report/report_invoice.xml",
+        "report/request_report.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3",
+}
