a411098ebc52742189a7d1b9818bccbbc5190183
Kung16845 <varit16845@gmail.com>
2026-08-27 15:01:18 +0700

    [T26-04535] Create Report Invoice Kaudy preprint

---
+++ spc_bs_apps/bs_create_form_tax_invoice_kuady_preprint/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    "name": "BS Create Form Tax Invoice Kuady Preprint",
+    "version": "17.0.0.1",
+    "author": "Basic-Solution Co., Ltd.",
+    "maintainer": "varit",
+    "website": "https://www.basic-solution.com/",
+    "category": "account",
+    "depends": [
+        "account",
+        "bs_invoice_modify_field",
+        "bs_manual_exchange_rate_ext",
+        "bs_multi_discount_accounting",
+    ],
+    "data": [
+        "report/components/report_header.xml",
+        "report/components/report_main_content.xml",
+        "report/components/report_footer.xml",
+        "report/report_invoice.xml",
+        "report/request_report.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3",
+}
