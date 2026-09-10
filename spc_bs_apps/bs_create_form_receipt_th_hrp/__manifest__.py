253f52465876e02a62b5f9004ae982c9462dad5f
Kung16845 <varit16845@gmail.com>
2026-09-10 14:37:56 +0700

    [T26-06133] Modify Receipt th

--- spc_bs_apps/bs_create_form_receipt_th_hrp/__manifest__.py
+++ spc_bs_apps/bs_create_form_receipt_th_hrp/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'BS Create Form Receipt TH HRP',
-    'version': '17.0.0.2',
+    'version': '17.0.0.3',
     'author': 'Basic-Solution Co., Ltd.',
     'website': 'https://www.basic-solution.com/',
     "license": "AGPL-3",
